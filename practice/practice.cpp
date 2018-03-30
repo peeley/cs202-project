ec0f7b3088682db70f6f87aa4fe675196f0eb168
tjbergstrom <tjbergstrom@alaska.edu>
2018-03-30 13:34:27 -0800

    adding practice.cpp with hello world

---
+++ practice/practice.cpp
@@ -0,0 +1,40 @@
+// practice.cpp
+// created 3/30/2018 by TB
+// this program is a test drive for 5 students working on one project using git
+// for practice only
+// add some code and edit the file if you want to get familiar with git
+// also checking for compatibility issues
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+using std::getline;
+#include <vector>
+using std::vector;
+#include <sstream>
+using std::istringstream;
+using std::ostringstream;
+using std::stringstream;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+using std::fstream;
+#include <cstdlib>
+#include <algorithm>
+#include <ios>
+
+
+
+int main(){
+	cout << "Hello world!" << endl;
+	return 0;
+}
+
+
+
+
+
+
