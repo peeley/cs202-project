9ed0b60aab4ee4229f3517d33ac3f4122dbd4fce
tjbergstrom <tjbergstrom@alaska.edu>
2018-04-01 12:22:40 -0800

    practicing with practice.cpp

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -36,22 +36,17 @@ int main(){
 	return 0;
 }
 
-void sfTest()
-{
+void sfTest(){
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
+    while (window.isOpen()){
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        window.clear();
+		window.clear();
         window.draw(shape);
         window.display();
     }
