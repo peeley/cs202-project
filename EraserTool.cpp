b0dc5b91569cbe3a8950294d1ad1dbb145d27fc8
Alexander Lewandowski <aflewandowski@alaska.edu>
2018-04-23 13:46:10 -0800

    eraser tool edit so it works properly w both tool button and hotkey

--- EraserTool.cpp
+++ EraserTool.cpp
@@ -3,16 +3,20 @@
 */
 #include "EraserTool.hpp"
 #include <iostream>
+
 void EraserTool::paintTick(Frame& f, sf::Color c, sf::Event& e) {
-	sf::CircleShape* circle;
-	circle = new sf::CircleShape(10);
-	sf::Vector2i position = sf::Mouse::getPosition(this->getWindow());
-	circle->setPosition(position.x-10, position.y-10);
-	circle->setFillColor(sf::Color::White);
-	f.addObject(*circle);
-	//this->playSound();            ///////////temp turn off link errors
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) || sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+		sf::CircleShape* circle;
+		circle = new sf::CircleShape(10);
+		sf::Vector2i position = sf::Mouse::getPosition(this->getWindow());
+		circle->setPosition(position.x - 10, position.y - 10);
+		circle->setFillColor(sf::Color::White);
+		f.addObject(*circle);
+		//this->playSound();            ///////////temp turn off link errors
+	}
 }
 void EraserTool::scrollAction(sf::Event& e){}
 EraserTool::EraserTool(sf::RenderWindow& w): Tool(w){
 	//this->initializeSound("assets/eraserEffect.wav");        /////////////////////temp turning off due to link errors
 }
+
